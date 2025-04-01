db80cfd275546cddfa5769ced9131fbaf29457d2
ArielPinochet <164126655+ArielPinochet@users.noreply.github.com>
2025-04-01 16:22:21 -0300

    Se inicializo Back Y frontEnd

---
+++ Backend/src/main/java/com/Tingeso1/Pep1Tingeso/Pep1TingesoApplication.java
@@ -0,0 +1,13 @@
+package com.Tingeso1.Pep1Tingeso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pep1TingesoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pep1TingesoApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/Tingeso1/Pep1Tingeso/Pep1TingesoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tingeso1.Pep1Tingeso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pep1TingesoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
