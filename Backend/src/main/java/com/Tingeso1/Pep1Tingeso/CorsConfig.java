4b6374ec640172e4a01221c6c6ab24a35432b777
ArielPinochet <ariel.pinochet@usach.cl>
2025-04-29 22:20:37 -0400

    porfavor funciona

--- Backend/src/main/java/com/Tingeso1/Pep1Tingeso/CorsConfig.java
+++ Backend/src/main/java/com/Tingeso1/Pep1Tingeso/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
