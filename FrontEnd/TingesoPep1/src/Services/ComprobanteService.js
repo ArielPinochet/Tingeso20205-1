4181d06daddafa72a55d1f492bcc1cf3251ac865
ArielPinochet <ariel.pinochet@usach.cl>
2025-04-30 16:25:42 -0400

    d

--- FrontEnd/TingesoPep1/src/Services/ComprobanteService.js
+++ FrontEnd/TingesoPep1/src/Services/ComprobanteService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://44.248.48.66:8090";
+const API_URL = "http://44.248.48.66:8090/comprobante-pago";
 
 
 const obtenerComprobantes = () => axios.get(API_URL);
