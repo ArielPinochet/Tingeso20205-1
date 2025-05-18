a97abc9352461e9e83d50f4fac0d767fff350ed9
ArielPinochet <ariel.pinochet@usach.cl>
2025-05-18 15:07:41 -0400

    Reestructuracion del codigo, y la iniciacion de proyecto 2

---
+++ PEP2/Tingeso.TarifaEspeciales/src/main/java/com/Pep2/Tingeso/TarifaEspeciales/Application.java
@@ -0,0 +1,13 @@
+package com.Pep2.Tingeso.TarifaEspeciales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
